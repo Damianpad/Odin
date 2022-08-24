cd90b0d6d27748b6e0f3b7f58868eba4be2c671b
Damian Padilla <damipad4@gmail.com>
2022-08-24 00:17:07 -0500

    Added exercises to numbers

--- Fundamentals/numbers.js
+++ Fundamentals/numbers.js
@@ -1,1 +1,82 @@
-console.log('Numbers Test');
+/*
+*********************
+Arithmetic Operations
+*********************
+A typical arithmetic operation operates on two numbers
+
+*/
+
+//The two numbers can be literals
+let x = 100 + 50;
+
+// or Variables
+let a;
+let b;
+let y = a + b;
+
+// or expressions
+
+let z = (100 + 50) * a;
+
+/*
+*********************
+Operators and Operands
+*********************
+The numbers (in an arithmetic operation) are called *operands*
+The operation (to be performed between the two operands) is defined by an operator
+
+Operand                 Operator                    Operand
+100                     +                           50
+
+*/
+
+/*
+******
+Adding
+******
+The addition operator (+) adds numbers
+*/
+
+let num1 = 5;
+let num2 = 2;
+let addingNums = num1 + num2;
+
+/*
+***********
+Subtracting
+***********
+The subtraction operator (-) subtracts numbers
+*/
+
+let subNum1 = 5;
+let subNum2 = 3;
+let subCalculation = subNum1 - subNum2;
+
+/*
+***********
+Multiplying
+***********
+The multiplication operator (*) multiplies numbers
+*/
+
+let multNum1 = 5;
+let multNum2 = 2;
+let multCalculation = multNum1 * multNum2;
+
+/*
+***********
+Dividing
+***********
+The division operator (/) divides numbers
+*/
+
+let divNum1 = 5;
+let divNum2 = 2;
+let divCalculation = divNum1 / divNum2;
+
+/*
+***********
+Remainder
+***********
+The modulus operator (%) returns the division remainder
+*/
