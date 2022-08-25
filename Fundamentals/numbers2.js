b8b1747919da1f742ae59f9873eb1af6dbb324ea
Damian Padilla <damipad4@gmail.com>
2022-08-25 02:47:05 -0500

    Adding part 2 of numbers in numbers2 js

---
+++ Fundamentals/numbers2.js
@@ -0,0 +1,9 @@
+console.log('Numbers 2 connected');
+
+// JavaScript has only one type of number. Numbers can be written with or without decimals
+let x = 3.14; // A number with decimals
+let y = 3; // A number without decimals
+
+// Extra long or extra small numbers can be written with scientific (exponent) notation:
+let largeNum = 123e5;
+let smallNum = 123e-5;
